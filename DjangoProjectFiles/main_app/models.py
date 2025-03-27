12cdbbd0e98624a66fc4d16812fc5fdd38675560
kirkseytc <kirkseytc@gmail.com>
2025-03-27 18:40:08 -0400

    product model impemented

--- DjangoProjectFiles/main_app/models.py
+++ DjangoProjectFiles/main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+#
+# This is the products model
+# Once we get a users model impemented 
+# we can uncomment the author member line
+#
+# for title and ISBN there are default values 
+# set up if none we passed in creation.
+#
+class Product(models.Model):
+    title = models.CharField("DEFAULT TITLE", max_length=250)
+    isbn = models.CharField("-------------", max_length=13)
+    # author = models.ForeignKey(Users, on_delete=models.CASCADE)
+    
+    
