c8a44c4178461f030130fa2055e1df283d630c7c
HayleeHauser <hayleebhauser@gmail.com>
2025-04-01 10:48:04 -0400

    User Model and Home HTML

--- DjangoProjectFiles/main_app/models.py
+++ DjangoProjectFiles/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,6 +14,6 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField("DEFAULT TITLE", max_length=250)
     isbn = models.CharField("-------------", max_length=13)
-    # author = models.ForeignKey(Users, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     
