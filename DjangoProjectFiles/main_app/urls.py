afe5a5af29175ff7dfe60014dd8fe4ce4053ab85
Tristan Kirksey <kirkseytc@gmail.com>
2025-04-08 00:05:55 -0400

    look at todo

--- DjangoProjectFiles/main_app/urls.py
+++ DjangoProjectFiles/main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('chats/', views.chat_hub, name='chat_hub'),
     path('chats/<str:pk>', views.chat_page, name='chats'),
 ]
--- DjangoProjectFiles/main_app/views.py
+++ DjangoProjectFiles/main_app/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from .models import Message
+from django.contrib.auth.decorators import login_required
+from .models import User, Message
 
 # Create your views here.
 
+@login_required
 def chat_page(request, pk):
 
-    # curr_user = request.user.
+    sender = request.user
+    receiver = User.objects.get(username=pk)
+    messages = Message.objects.filter(sender=sender, receiver=receiver).values() | Message.objects.filter(sender=receiver, receiver=sender).values() 
 
-    return render(request, 'base/chat_room.html')
+    # todo: handle adding message to model
+
+    context = {
+        'sender': sender,
+        'receiver': receiver,
+        'messages': messages
+    }
+    return render(request, 'base/chat_room.html', context)
+
+@login_required
+def chat_hub(request):
+
+    return render(request, 'base/chat_hub.html')
