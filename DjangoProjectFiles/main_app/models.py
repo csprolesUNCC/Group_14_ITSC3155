125ea5102292e8735adcc8a4f64e88fab88e3567
parkerJenkins36 <pjenki17@uncc.edu>
2025-04-09 13:44:32 -0700

    create_listings

--- DjangoProjectFiles/main_app/models.py
+++ DjangoProjectFiles/main_app/models.py
@@ -17,3 +17,25 @@ class Product(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField("DEFAULT DESCRIPTION", max_length=512)
     
+
+CONDITION_CHOICES = [
+    ('new', 'New'),
+    ('like_new', 'Like New'),
+    ('good', 'Good'),
+    ('acceptable', 'Acceptable'),
+    ('poor', 'Poor'),
+]
+
+class Listing(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    textbook_name = models.CharField(max_length=200)
+    college = models.CharField(max_length=200)
+    course = models.CharField(max_length=100)
+    class_name = models.CharField(max_length=200)
+    condition = models.CharField(max_length=20, choices=CONDITION_CHOICES)
+    teacher = models.CharField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='listing_images/', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.textbook_name
--- DjangoProjectFiles/main_app/views.py
+++ DjangoProjectFiles/main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='login')
+def createListing(request):
+    form = ListingForm()
+    if request.method == 'POST':
+        form = ListingForm(request.POST, request.FILES)
+        if form.is_valid():
+            listing = form.save(commit=False)
+            listing.seller = request.user
+            listing.save()
+            return redirect('home')  # or a listing confirmation page
+    return render(request, 'base/create_listing.html', {'form': form})
+
