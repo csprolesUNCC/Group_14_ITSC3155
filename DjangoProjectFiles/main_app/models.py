0e4e885af6296a5d8b3da07a083e3216a1b7b31b
HayleeHauser <hayleebhauser@gmail.com>
2025-04-14 16:02:22 -0400

    home page stuff

--- DjangoProjectFiles/main_app/models.py
+++ DjangoProjectFiles/main_app/models.py
@@ -11,9 +11,18 @@ from django.contrib.auth.models import User
 # for title and ISBN there are default values 
 # set up if none we passed in creation.
 #
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     title = models.CharField("DEFAULT TITLE", max_length=250)
     isbn = models.CharField("-------------", max_length=13)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.CharField("DEFAULT DESCRIPTION", max_length=512)
-    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField("DEFAULT DESCRIPTION", max_length=512)
+
+
+
