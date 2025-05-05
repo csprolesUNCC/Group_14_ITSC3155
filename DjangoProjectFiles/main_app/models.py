a3ed571a95a11c2d3fb3b9537eb5ab485cb238c4
Tristan Kirksey <kirkseytc@gmail.com>
2025-05-05 18:14:46 -0400

    isbn field added

--- DjangoProjectFiles/main_app/models.py
+++ DjangoProjectFiles/main_app/models.py
@@ -37,7 +37,7 @@ class Listing(models.Model):
     image = models.ImageField(upload_to='listing_images/', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    
+    isbn = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.textbook_name
